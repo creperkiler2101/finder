b4b50bb87bd27cdbbe0f1050edaf64072318e7c5
creperkiler2101 <50651572+creperkiler2101@users.noreply.github.com>
2019-09-26 11:14:35 +0300

    Add files via upload

--- github finder/app.js
+++ github finder/app.js
@@ -1,56 +1,115 @@
-const oDiv = "<div>";
-const cDiv = "</div>";
+const searchElement = document.getElementById("search");
+const resultElement = document.getElementById("result");
 
-const searchId = "search";
-const resultId = "result";
-const apiLink = "https://api.github.com";
-const userLink = "/search/users?q=";
+//Set up event handlers
+searchElement.onkeyup = search;
 
-const searchElement = document.getElementById(searchId);
-const resultElement = document.getElementById(resultId);
-
-class ResultBlock {
+//---------------------
+class GithubUser {
     constructor() {
         this.avatarURL = "";
         this.userURL = "";
         this.userName = "";
     }
 
-    toHTMLString() {
+    toHTML() {
         let result = "";
-        result += oDiv;
-        result += `<img style="width: 50px; height: 50px;" src='${this.avatarURL}' alt=''>`;
         result += `<a href='${this.userURL}'>${this.userName}</a>`;
-        result += cDiv;
-
+        result += "<br>";
         return result;
     }
 }
 
-function getJSON(url, callback) {
-    let xmlRequest = new XMLHttpRequest();
-    xmlRequest.responseType = "JSON";
-    xmlRequest.open("GET", url);
-    xmlRequest.onload = function() {
-        callback(xmlRequest.status, JSON.parse(xmlRequest.response));
+class GithubRepository {
+    constructor() {
+        this.name = "";
+        this.url = "";
+        this.description = "";
     }
-    xmlRequest.send();
+
+    toHTML() {
+        let result = "";
+        result += "<div>";
+        result += `<a href='${this.url}'>${this.name}</a>`;
+        result += "</div>";
+        return result;
+    }
 }
+
+
+class Github {
+    constructor(url, clientId, secretId, maxUsers, maxRepos) {
+        this.url = url;
+        this.maxUsers = maxUsers;
+        this.maxRepos = maxRepos;
+        this.clientId = clientId;
+        this.secretId = secretId;
+    }
+
+    getJSON(url, callback) {
+        let xmlRequest = new XMLHttpRequest();
+        xmlRequest.responseType = "JSON";
+        xmlRequest.open("GET", url);
+        xmlRequest.onload = function() {
+            callback(xmlRequest.status, JSON.parse(xmlRequest.response));
+        }
+        xmlRequest.send();
+    }
+
+    getUsers(query, callback) {
+        this.getJSON(`${this.url}search/users?q=${query}&client_id=${this.clientId}&client_secret=${this.secretId}&per_page=${this.maxUsers}`, function(status, data) {
+            if (status === 200) {
+                let result = [];
+                data.items.forEach(function(value) {
+                    let user = new GithubUser();
+                    user.userName = value.login;
+                    user.avatarURL = value.avatar_url;
+                    user.userURL = value.html_url;
+                    result.push(user);
+                });
+                callback(status, result);
+            }
+        });
+    }
+
+    getRepositories(user, callback) {
+        console.log(`${this.url}users/${user.userName}/repos?client_id=${this.clientId}&client_secret=${this.secretId}&per_page=${this.maxRepos}`);
+        this.getJSON(`${this.url}users/${user.userName}/repos?client_id=${this.clientId}&client_secret=${this.secretId}&per_page=${this.maxRepos}`, function(status, data) {
+            if (status === 200) {
+                let result = [];
+                data.forEach(function(value) {
+                    let repos = new GithubRepository();
+                    repos.name = value.name;
+                    repos.url = value.html_url;
+                    repos.description = value.description;
+                    result.push(repos);
+                });
+                callback(status, result);
+            }
+        });
+    }
+}
+
+const github = new Github("https://api.github.com/", "8d42f4dc753eb6f1b180", "98222f8d8d60ffe189cb34cb65ad3025f1618b3d", 10, 5);
 
 function search() {
-    let elementsResult = "";
-    getJSON(apiLink + userLink + searchElement.value, function(status, result) {
-        result.items.forEach(function(value) {
-            let block = new ResultBlock();
-            block.avatarURL = value.avatar_url;
-            block.userURL = value.html_url;
-            block.userName = value.login;
-            elementsResult += block.toHTMLString();
-        });
-
-        resultElement.innerHTML = elementsResult;
+    github.getUsers(searchElement.value, function(status, result) {
+        if (status === 200) {
+            let htmlString = "";
+            result.forEach(function(value) {
+                htmlString += "<div>";
+                htmlString += value.toHTML();
+                github.getRepositories(value, function(status, result) {
+                    result.forEach(function(value) {
+                        htmlString += value.toHTML();
+                    });
+                });
+                htmlString += "</div>";
+            });
+            resultElement.innerHTML = htmlString;
+        } else {
+            resultElement.innerHTML = "Nothing founded";
+        }
     });
 }
-
-searchElement.onkeyup = search;
 
